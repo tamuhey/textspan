e266c51638c51b751fa53d6c8b5c219976738c15
Yohei Tamura <tamuhey@gmail.com>
2021-02-05 13:28:33 +0900

    [versionup] 0.4.2 -> 0.5.0

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -3,7 +3,7 @@ use textspan::Span;
 
 #[pymodule]
 fn textspan(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add("__version__", "0.4.2")?;
+    m.add("__version__", "0.5.0")?;
 
     /// Converts the spans defined in `text` to those defined in `original_text`.
     ///
